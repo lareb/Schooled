a965682a08f25d9382baf416a926885ad71a4a89
Kristiyan96 <kristiyan.c@gmail.com>
2017-08-21 15:51:58 +0300

    Adding grades to different terms

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -1,8 +1,12 @@
 $(function() {
+	var used=false;
     $('.give-marks a.new').click(function(){
-    	mark  = $(this).html();
-    	$(this).parent().parent().siblings('.mark_mark')[0].firstChild.value=mark;
-    	var form = $(this).parent().parent().parent();
-    	form.submit();
+    	if(used==false){
+	    	mark  = $(this).html();
+	    	$(this).parent().parent().siblings('.mark_mark')[0].firstChild.value=mark;
+	    	var form = $(this).parent().parent();
+	    	form.submit();
+	    	used=true;
+	    }
     });
 });
