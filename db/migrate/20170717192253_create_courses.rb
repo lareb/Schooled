3f3e56e1060bd72981eca447492d55a7296f1a19
Kristiyan96 <kristiyan.c@gmail.com>
2017-08-23 02:10:09 +0300

    Displaying absences and fixes in migrations

--- db/migrate/20170717192253_create_courses.rb
+++ db/migrate/20170717192253_create_courses.rb
@@ -7,6 +7,7 @@ class CreateCourses < ActiveRecord::Migration[5.1]
     	t.references :user,           index: true
       t.integer :year,              null: false
       t.integer :grade,             null: false
+      t.integer :study_hours,       null: false
     end
   end
 end
