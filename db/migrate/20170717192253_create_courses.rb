871680d748d21736bf9095ba0b4e4535bbc3504c
Kristiyan96 <kristiyan.c@gmail.com>
2017-08-23 01:54:33 +0300

    Added absences to students

--- db/migrate/20170717192253_create_courses.rb
+++ db/migrate/20170717192253_create_courses.rb
@@ -4,7 +4,7 @@ class CreateCourses < ActiveRecord::Migration[5.1]
     	t.references :subject,        index: true
     	t.references :school,         index: true
     	t.references :group,          index: true
-    	t.references :user,           undex: true
+    	t.references :user,           index: true
       t.integer :year,              null: false
       t.integer :grade,             null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170819175119) do
+ActiveRecord::Schema.define(version: 20170822224354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "absences", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "course_id"
+    t.integer "value", default: 1, null: false
+    t.integer "purpose", default: 0, null: false
+    t.boolean "excused", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_absences_on_course_id"
+    t.index ["user_id"], name: "index_absences_on_user_id"
+  end
 
   create_table "courses", force: :cascade do |t|
     t.bigint "subject_id"
